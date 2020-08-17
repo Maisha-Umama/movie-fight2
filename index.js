1131878ebe167ba747d43754f95599442efd290a
Maisha Umama <umamamaisha@gmail.com>
2020-08-17 17:05:01 +0600

    completed two search box

--- index.js
+++ index.js
@@ -1,43 +1,59 @@
-createAutoComplete({
-root :document.querySelector('#left-autocomplete'),
-renderOption(movie){
-  const imgSrc = movie.Poster ==='N/A' ? '' : movie.Poster;
-  return `
-  <img src ="${imgSrc}" />
-  ${movie.Title}(${movie.Year})
-  `;
-
-},
- onOptionSelect(movie){
-   onMovieSelect(movie); 
- },
- inputValue(movie){
-   return movie.Title;
- },
- async fetchData(searchTerm){
-  const response = await axios.get('http://www.omdbapi.com/', {
-    params: {
-      apikey: '4ac4d032',
-      s: searchTerm
-    } 
-  });//to fetch data about the search you made
+const autoCompleteConfig= {
+  renderOption(movie){
+    const imgSrc = movie.Poster ==='N/A' ? '' : movie.Poster;
+    return `
+    <img src ="${imgSrc}" />
+    ${movie.Title}(${movie.Year})
+    `;
    
-  if(response.data.Error){
-    return []; 
+  },
+ 
+   inputValue(movie){
+     return movie.Title;
+   },
+   async fetchData(searchTerm){
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: '4ac4d032',
+        s: searchTerm
+      } 
+    });//to fetch data about the search you made
+     
+    if(response.data.Error){
+      return []; 
+    }
+   return response.data.Search ;
   }
- return response.data.Search ;
+
+};
+
+createAutoComplete({
+  ...autoCompleteConfig,
+root :document.querySelector('#left-autocomplete'),
+onOptionSelect(movie){
+  document.querySelector('.tutorial').classList.add('is-hidden');
+  onMovieSelect(movie,document.querySelector('#left-summary') ); 
 }
+
+}); 
+createAutoComplete({
+  ...autoCompleteConfig,
+root :document.querySelector('#right-autocomplete'),
+onOptionSelect(movie){
+  document.querySelector('.tutorial').classList.add('is-hidden');
+  onMovieSelect(movie, document.querySelector('#right-summary')); 
+}
+
 }); 
 
-
-const onMovieSelect= async movie =>{
+const onMovieSelect= async (movie,summaryElement) =>{
   const response=await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: '4ac4d032',
       i: movie.imdbID
     } 
   });
- document.querySelector('#summary').innerHTML=movieTemplate(response.data);
+ summaryElement.innerHTML=movieTemplate(response.data);
 };
 
 const movieTemplate = (movieDetail) => {
